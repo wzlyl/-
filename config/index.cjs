51a3dcc01222fd420a37bea60b27ecd1ebdbdb5e
wzlyl <111719870+wzlyl@users.noreply.github.com>
2024-01-15 11:19:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,26 +6,26 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '威海',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU27768Ty8bKsKPDv3177ruN5qxg5c8NQBXONyGd',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-25',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-10-04' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
